bec170e7558481fb9a73fd2b18c57ef2f9cf03db
RnagerusGenome <trevorkuhlmannk@gmail.com>
2022-02-09 15:29:26 +0200

    Add project files.

---
+++ BmwTestTrevor/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace BmwTestTrevor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Source = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SourceTextBox = new System.Windows.Forms.TextBox();
+            this.DestinationTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ReplicateButton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 2;
+            // 
+            // Source
+            // 
+            this.Source.AutoSize = true;
+            this.Source.Location = new System.Drawing.Point(18, 78);
+            this.Source.Name = "Source";
+            this.Source.Size = new System.Drawing.Size(50, 16);
+            this.Source.TabIndex = 3;
+            this.Source.Text = "Source";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Destination ";
+            // 
+            // SourceTextBox
+            // 
+            this.SourceTextBox.Location = new System.Drawing.Point(101, 72);
+            this.SourceTextBox.Name = "SourceTextBox";
+            this.SourceTextBox.Size = new System.Drawing.Size(326, 22);
+            this.SourceTextBox.TabIndex = 5;
+            // 
+            // DestinationTextBox
+            // 
+            this.DestinationTextBox.Location = new System.Drawing.Point(101, 142);
+            this.DestinationTextBox.Name = "DestinationTextBox";
+            this.DestinationTextBox.Size = new System.Drawing.Size(326, 22);
+            this.DestinationTextBox.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(613, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(613, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ReplicateButton
+            // 
+            this.ReplicateButton.Location = new System.Drawing.Point(21, 263);
+            this.ReplicateButton.Name = "ReplicateButton";
+            this.ReplicateButton.Size = new System.Drawing.Size(121, 37);
+            this.ReplicateButton.TabIndex = 0;
+            this.ReplicateButton.Text = "Replicate";
+            this.ReplicateButton.Click += new System.EventHandler(this.ReplicateButton_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 383);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 37);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "View Results";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(412, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ReplicateButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DestinationTextBox);
+            this.Controls.Add(this.SourceTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Source);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Trevor Kuhlmann Bmw Exercise";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Source;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox SourceTextBox;
+        private System.Windows.Forms.TextBox DestinationTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ReplicateButton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BmwTestTrevor/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BmwTestTrevor
+{
+    public partial class Form1 : Form
+    {
+
+
+        /// <summary>
+        /// declare source path and detination patth variables 
+        /// </summary>
+        string SourcePath = "";
+        string DestinationPath = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+
+            folder.ShowDialog();
+            String path = folder.SelectedPath;
+            SourceTextBox.Text = path;
+            SourcePath = path;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+
+            folder.ShowDialog();
+            String path = folder.SelectedPath;
+            DestinationTextBox.Text = path;
+            DestinationPath = path;
+
+
+        }
+
+
+
+        private void ReplicateButton_Click_1(object sender, EventArgs e)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            System.IO.DirectoryInfo dir1 = new System.IO.DirectoryInfo(SourcePath);
+            System.IO.DirectoryInfo dir2 = new System.IO.DirectoryInfo(DestinationPath);
+
+            
+
+            IEnumerable<System.IO.FileInfo> list1 = dir1.GetFiles("*.*",
+            System.IO.SearchOption.AllDirectories);
+
+            IEnumerable<System.IO.FileInfo> list2 = dir2.GetFiles("*.*",
+            System.IO.SearchOption.AllDirectories);
+
+
+
+
+
+            bool IsInDestination = false;
+            bool AppearsInDestinationButNotSource = false;
+           
+
+            //First condition : File not present in destination 
+
+            foreach (System.IO.FileInfo a in list1)
+            {
+               
+
+                foreach (System.IO.FileInfo s2 in list2)
+                {
+                    if (a.Name == s2.Name)
+                    {
+                        IsInDestination = true;
+                        break;
+                    }
+                    else
+                    {
+                        IsInDestination = false;
+                    }
+                }
+
+                if (IsInDestination == false)
+                {
+                    System.IO.File.Copy(a.FullName, System.IO.Path.Combine(DestinationPath, a.Name), true);
+                    sb.AppendLine (  "copied the following file destination folder because it did not exist :" + dir2 + " filename" + a.Name);
+
+                }
+            }
+
+
+                ///second condition 
+                ///If a files is different in size or date and time then it should be replaced.
+                /////
+
+                foreach (System.IO.FileInfo b in list1)
+                {
+
+
+                    foreach (System.IO.FileInfo s2 in list2)
+                    {
+                        if (b.Name == s2.Name && ((b.CreationTime != s2.CreationTime) || (b.Length != s2.Length)))
+                        {
+                            System.IO.File.Copy(b.FullName, System.IO.Path.Combine(DestinationPath, b.Name), true);
+                            sb.AppendLine("overwrote the following files of different size and timestamp to destination folder :" + dir2 + " filename" + b.Name);
+                    }
+
+                    }
+                }
+
+                ///third condition
+                ///If a file exists in the destination but not the source it must be deleted from the destination.
+
+
+                foreach (System.IO.FileInfo dest in list2)
+                {
+
+
+                    foreach (System.IO.FileInfo srce in list1)
+                    {
+                    if (dest.Name == srce.Name)
+                    {
+
+                        AppearsInDestinationButNotSource = false;
+                        break;
+                  
+
+                    }
+
+                    else {
+                        AppearsInDestinationButNotSource = true;
+                    }
+
+                    }
+
+
+                if (AppearsInDestinationButNotSource == true)
+
+                {
+
+                    string v = dest.FullName;
+                   
+
+                    System.IO.File.Delete(v);
+                    sb.AppendLine("Deleted the following files that existed in destination but not source :" + dir2 + " filename" + v);
+                }
+
+                }
+            
+
+            ///replicate folders 
+
+            CustomSearcher cs = new CustomSearcher();
+            
+            var sourcedir = CustomSearcher.GetDirectories(SourcePath);
+
+            var destedir = CustomSearcher.GetDirectories(DestinationPath);
+
+
+
+            foreach (string v in sourcedir)
+
+
+
+            {
+
+                if (destedir.Count > 0 )
+                {
+
+                    foreach (string b in destedir)
+
+                    {
+
+
+                        string sc = v.Replace(dir1.ToString(), "**");
+
+                        string ds = b.Replace(dir2.ToString(), "**");
+
+                        if (sc != ds)
+
+                        {
+                            Directory.CreateDirectory(sc.Replace("**", dir2.ToString()));
+                            sb.AppendLine("created the following directries that did not exist in destination :" + sc.Replace("**", dir2.ToString()));
+
+                        }
+                    }
+                }
+                else {
+
+                    foreach (string q in sourcedir) {
+
+
+                        string sc = q.Replace(dir1.ToString(), "**");
+
+                     
+
+
+                        Directory.CreateDirectory(sc.Replace("**", dir2.ToString()));
+
+                        sb.AppendLine("created the following directries that did not exist in destination :" + sc.Replace("**", dir2.ToString()));
+
+                    }
+
+                }
+
+                }
+
+
+
+            using (StreamWriter writer = new StreamWriter(@"c:\resultsTrevorExercise.txt"))
+            {
+                writer.WriteLine(sb);
+       
+              
+            }
+            label4.ForeColor = Color.Red;
+           
+            label4.Text = "COMPLETE !";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"c:\resultsTrevorExercise.txt");
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+                    
+        
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+                    
+                
+            
+        
+
+
+
+
+
